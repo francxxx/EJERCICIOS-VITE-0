df2e8aa3963b071b80c79b098bbe35cca82bc6b3
francxxx <francocedano35@gmail.com>
2026-06-15 13:10:07 +0200

    aja

--- src/assets/js/assets/_artJs.js
+++ src/assets/js/assets/_artJs.js
@@ -7,42 +7,70 @@ miotravariable = "Aranaz"
 
 let num1 = 20
 let num2 = 8
-let resultado = num1 + num2
+let resultado = num1 * num2
 
 
 console.log(mivariable + "" + miotravariable)
 console.log(resultado)
 
 //const elementoHtmlBoton = document.getElementById("boton")
-const elementoHtmlBoton = document.querySelector("#IDboton")
+const elementoHtmlBoton = document.querySelector("#botonID")
 
 //elementoHtmlBoton.style.backgroundColor="pink"
 
 // console.log(elementoHtmlBoton)
 
 
-elementoHtmlBoton.addEventListener("click", function(){
-    elementoHtmlBoton.style.backgroundColor="orange"
+elementoHtmlBoton.addEventListener("click", function(event){
+    event.currentTarget.style.backgroundColor="pink"
     elementoHtmlBoton.style.color="black"
 })
 
 const botones = document.querySelectorAll(".boton")
 
 botones.forEach(function(boton){
-    boton.addEventListener("click", function(){
+    boton.addEventListener("click", function(e){
         boton.style.backgroundColor="pink"
         boton.style.color="green"
     })
 
 })
+
+
+
 
 botones.forEach((boton)=>{
-    boton.addEventListener("click", ()=>{
+
+    boton,addEventListener("click" , (e)=>{
+
+        botones.forEach((boton)=>{
+            boton.style.backgroundColor="#410a6e"
+            boton.style.border="2px dashed #410a6e"
+            boton.style.color="white"
+            console.log(mivariable)
+            })
+
+        e.currentTarget.style.border="2px solid green"
         boton.style.backgroundColor="pink"
         boton.style.color="green"
+
     })
 
 })
+
+const botonesDesplegar = document.querySelectorAll(".botonDesplegar")
+const parrafoADesplegar = document.querySelector(".desplegable")
+
+    botonesDesplegar.forEach((item)=>{
+
+        item.addEventListener("click" , ()=>{
+            parrafoADesplegar.classList.toggle("open")
+        })
+    })
+
+
+
+
 
 
 
